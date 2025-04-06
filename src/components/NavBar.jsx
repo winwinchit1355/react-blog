0c7d131895fed4077c0ab2fa6b725999b995b69e
winwinchit1355 <76089452+winwinchit1355@users.noreply.github.com>
2025-04-06 23:11:05 +0630

    add navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div>
+    <div className='w-full h-16 md:h-20 flex items-center justify-between'>
         {/* LOGO  */}
+        <div className="flex items-center gap-4 text-2xl font-bold">
+          <img src="/images/logo.png" alt="" className='w-10 h-10 md:w-15 md:h-15 rounded-full object-cover' />
+          <span>My Journey</span>
+        </div>
         {/* MOBILE MENU  */}
+        <div className="md:hidden">
+          {menuOpen ? (
+            <button onClick={() => setMenuOpen(false)} className="text-2xl cursor-pointer">X</button>
+          ) : (
+            <button onClick={() => setMenuOpen(true)} className="text-2xl cursor-pointer">☰</button>
+          )}
+        </div>
         {/* DESKTOP MENU  */}
+        <div className="hidden md:flex">D</div>
     </div>
   )
 }
